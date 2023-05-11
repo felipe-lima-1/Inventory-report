ef41c8872d4c10cae073c81702d2b3c55f371073
felipe-lima-1 <luizbrutante@gmail.com>
2023-05-11 15:17:30 -0300

    Requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        "1",
+        "NITROUS OXIDE",
+        "Galena Biopharma",
+        "2020-12-22",
+        "2024-11-07",
+        "CZ09 8588 0858 8435 9140 2695",
+        "instrucao 3",
+    )
+    assert produto.id == "1"
+    assert produto.nome_do_produto == "NITROUS OXIDE"
+    assert produto.nome_da_empresa == "Galena Biopharma"
+    assert produto.data_de_fabricacao == "2020-12-22"
+    assert produto.data_de_validade == "2024-11-07"
+    assert produto.numero_de_serie == "CZ09 8588 0858 8435 9140 2695"
+    assert produto.instrucoes_de_armazenamento == "instrucao 3"
