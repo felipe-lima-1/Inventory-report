e7c17b196f94157ad5febf7a2d8c87dd5fc94f5a
felipe-lima-1 <luizbrutante@gmail.com>
2023-05-11 16:40:13 -0300

    Requisito 6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -13,12 +14,23 @@ def read_json(file):
     return json.loads(data)
 
 
+def read_xml(file):
+    root = ET.parse(file).getroot()
+    data = [
+        {tag.tag: tag.text for tag in record}
+        for record in root.findall(".//record")
+    ]
+    return data
+
+
 def read_file(file_path):
     with open(file_path) as file:
         if file_path.endswith(".csv"):
             return read_csv(file)
         elif file_path.endswith(".json"):
             return read_json(file)
+        elif file_path.endswith(".xml"):
+            return read_xml(file)
 
 
 class Inventory:
