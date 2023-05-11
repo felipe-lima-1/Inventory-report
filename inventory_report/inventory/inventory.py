943939f3a6c18e692f2e40aaef24424ff6e586a0
felipe-lima-1 <luizbrutante@gmail.com>
2023-05-11 16:29:47 -0300

    Requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -7,10 +8,17 @@ def read_csv(file):
     return list(csv.DictReader(file))
 
 
+def read_json(file):
+    data = file.read()
+    return json.loads(data)
+
+
 def read_file(file_path):
     with open(file_path) as file:
         if file_path.endswith(".csv"):
             return read_csv(file)
+        elif file_path.endswith(".json"):
+            return read_json(file)
 
 
 class Inventory:
