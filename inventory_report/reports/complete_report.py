10eca9ccd465ed3bf84fc46adcb2a66a7b3cbdb4
felipe-lima-1 <luizbrutante@gmail.com>
2023-05-11 16:03:05 -0300

    Requisito 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,14 @@
+from .simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(data):
+        company_items = Counter(
+            [product["nome_da_empresa"] for product in data]
+        )
+        company_report = "\nProdutos estocados por empresa:\n"
+        for company, count in company_items.items():
+            company_report += f"- {company}: {count}\n"
+        return SimpleReport.generate(data) + company_report
