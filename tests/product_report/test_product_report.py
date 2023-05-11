a41ce370bc7c2ce4b02b14119c4cb81f4c71b61a
felipe-lima-1 <luizbrutante@gmail.com>
2023-05-11 17:02:23 -0300

    Requisito 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        "1",
+        "NITROUS OXIDE",
+        "Galena Biopharma",
+        "2020-12-22",
+        "2024-11-07",
+        "CZ09 8588 0858 8435 9140 2695",
+        "instrucao 3",
+    )
+
+    assert produto.__repr__() == (
+        f"O produto {produto.nome_do_produto}"
+        f"fabricado em {produto.data_de_fabricacao}"
+        f"por {produto.nome_da_empresa}"
+        f"com validade até {produto.data_de_validade}"
+        f"precisa ser armazenado {produto.instrucoes_de_armazenamento}"
+    )
